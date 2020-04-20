ef0482b35f4247daa64fcb9f49a9113b5cfd7585
Thomas Perkins <thomasperkins1123@gmail.com>
2020-04-21 00:26:43 +0100

    Temp commit

--- .codebuild/github-post-pr.sh
+++ .codebuild/github-post-pr.sh
@@ -5,23 +5,21 @@
 # arg2 URL to catfood branch
 
 set -euvo pipefail
-COVER_FULL_SHA=4d5ce0ace7a22400b693d8bbf5547c1266c839b7
+COVER_FULL_SHA=$1
 CATFOOD_URL=$2
 
 # get PR number from commit
 PR=$(curl -s GET \
    -H "Authorization: token ${GITHUB_CI_STATUS_TOKEN}" \
    -H "Accept: application/vnd.github.groot-preview+json" \
-   https://api.github.com/repos/diffblue/CoreBanking/commits/$COVER_FULL_SHA/pulls
+   https://api.github.com/repos/diffblue/CoreBanking/commits/$COVER_FULL_SHA/pulls | jq --arg COVER_FULL_SHA "$COVER_FULL_SHA" '.[] | select(.head.sha == $COVER_FULL_SHA) | .url' | sed 's~^.*\/\([0-9]*\)"$~\1~' || true
 )
 
 echo "$PR"
 # post catfood branch link to PR
-COVER_BRANCH_URL=https://api.github.com/repos/diffblue/CoreBanking/issues/$PR/comments
+COVER_BRANCH_URL=https://api.github.com/repos/diffblue/CoreBanking/issues/"$PR"/comments
 curl --location --request POST "${COVER_BRANCH_URL}" \
-  
-	
-  #--header "Content-Type: application/json" \
-  #--header "Authorization: Bearer ${GITHUB_CI_STATUS_TOKEN}" \
-  #--header "Content-Type: application/json" \
-  #--data-raw "{\"body\": \"test\"}"
+  --header "Content-Type: application/json" \
+  --header "Authorization: Bearer ${GITHUB_CI_STATUS_TOKEN}" \
+  --header "Content-Type: application/json" \
+  --data-raw "{\"body\": \"test\"}"
