927754fae17ae86278065624a62d4305350aaa57
Thomas Perkins <thomasperkins1123@gmail.com>
2020-04-20 18:14:55 +0100

    Make codebuild now comment on PRs

---
+++ .codebuild/getTestOutput.py
@@ -0,0 +1,17 @@
+import sys
+
+foundResults = False
+foundTestsRun = False
+
+for line in sys.argv[1].split("\n"):
+    if "Results :" == line and not foundTestsRun:
+        foundResults = True
+    if "Tests run:" in line and "Time elapsed:" not in line:
+        foundTestsRun = True
+        foundResults = False
+        print(line)
+    if foundResults and "expected" in line:
+        #if "Failed tests:   " in line:
+            #print("Failed Tests:")
+        line = line.replace("Failed tests: ", "")
+        #print(line.split("(")[0] +  "() from " + line.split("(")[1].split(")")[0] + "")
---
+++ .codebuild/github-post-pr.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/env bash
+
+# Posts a comment on the github page of the cover PR that triggered the current build.
+# arg1 Cover commit SHA
+# arg2 URL to catfood branch
+
+set -euvo pipefail
+COVER_FULL_SHA=$1
+MESSAGE=$2
+echo "$MESSAGE"
+
+# get PR number from commit
+PR=$(curl -s GET \
+   -H "Authorization: token ${GITHUB_CI_STATUS_TOKEN}" \
+   -H "Accept: application/vnd.github.groot-preview+json" \
+   https://api.github.com/repos/diffblue/CoreBanking/commits/$COVER_FULL_SHA/pulls | jq --arg COVER_FULL_SHA "$COVER_FULL_SHA" '.[] | select(.head.sha == $COVER_FULL_SHA) | .url' | sed 's~^.*\/\([0-9]*\)"$~\1~' || true
+)
+
+echo "$PR"
+# post catfood branch link to PR
+COVER_BRANCH_URL=https://api.github.com/repos/diffblue/CoreBanking/issues/"$PR"/comments
+curl --location --request POST "${COVER_BRANCH_URL}" \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Bearer ${GITHUB_CI_STATUS_TOKEN}" \
+  --header "Content-Type: application/json" \
+  --data-raw "{\"body\": \"$MESSAGE\"}"
