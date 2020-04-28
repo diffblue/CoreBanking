ab1d561f7312537922890df67e35e1d7a14ce41e
Thomas Perkins <thomasperkins1123@gmail.com>
2020-04-28 22:26:45 +0100

    Stop pushing to my own branch

--- .codebuild/dogfooding-demo.sh
+++ .codebuild/dogfooding-demo.sh
@@ -4,11 +4,16 @@ set -euvo pipefail
 
 BRANCH="$1"
 SHA="$2"
+mvn clean compile -B 
+timeout 360 ./dcover create -d src/dcover/java --skip-test-verification --batch
 
-git add src/dcover
-git commit -m "Add diffblue tests" || true
-git push origin "$BRANCH"
-NEW_SHA=$(git rev-parse HEAD)
-echo "$SHA"
-echo "$NEW_SHA"
-.codebuild/github-post-pr.sh "$SHA" "Diffblue has found some code changes please check these are intended $NEW_SHA"
+if [ "$BRANCH" != "dcover-dogfooding-master" ]
+then
+	git add src/dcover
+	git commit -m "Add diffblue tests" || true
+	git push origin "$BRANCH"
+	NEW_SHA=$(git rev-parse HEAD)
+	echo "$SHA"
+	echo "$NEW_SHA"
+	.codebuild/github-post-pr.sh "$SHA" "Diffblue has found some code changes please check these are intended $NEW_SHA"
+fi
