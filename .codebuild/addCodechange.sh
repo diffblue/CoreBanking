752e7293f2a332863b4b9af835826c3347ad7535
Thomas Perkins <thomasperkins1123@gmail.com>
2020-04-29 23:28:04 +0100

    Add codebuild change updates

--- .codebuild/addCodechange.sh
+++ .codebuild/addCodechange.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-masterBranch="dcover-dogfooding-master"
+masterBranch="dcover-dogfooding-noTrivial"
 
 if [ $(git config --get remote.origin.url)"" == "git@github.com:diffblue/CoreBanking.git" ]; then
 	if [ $(git name-rev --name-only HEAD)"" == "$masterBranch" ]; then
