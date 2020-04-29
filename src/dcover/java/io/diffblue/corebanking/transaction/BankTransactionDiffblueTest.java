2a7ba8c60996cbe23bdb0dc1e4e65c8c4bf816ea
Thomas Perkins <thomasperkins1123@gmail.com>
2020-04-29 23:32:21 +0100

    Remove trivial tests

--- src/dcover/java/io/diffblue/corebanking/transaction/BankTransactionDiffblueTest.java
+++ src/dcover/java/io/diffblue/corebanking/transaction/BankTransactionDiffblueTest.java
@@ -7,25 +7,6 @@ import java.util.Date;
 import org.junit.Test;
 
 public class BankTransactionDiffblueTest {
-  @Test
-  public void testConstructor() {
-    // Arrange
-    Date date = new Date(1L);
-
-    // Act
-    BankTransaction actualBankTransaction = new BankTransaction(10L, date,
-        new Account(1234567890L, new Client(""), 10L), new Account(1234567890L, new Client(""), 10L));
-
-    // Assert
-    assertEquals(Transaction.TransactionState.NOT_EXECUTED_YET, actualBankTransaction.getTransactionState());
-    assertEquals("1234567890", actualBankTransaction.getSource());
-    assertEquals(
-        "Transaction: | 70.01.01\t| Source: 1234567890\t| Target:"
-            + " 1234567890\t| Amount: 10\t| Balance: 0\t| Transaction" + " state: NOT_EXECUTED_YET\t|",
-        actualBankTransaction.toString());
-    assertEquals("1234567890", actualBankTransaction.getTarget());
-    assertEquals(10L, actualBankTransaction.getTransactionAmount());
-  }
 
   @Test
   public void testGetSource() {
