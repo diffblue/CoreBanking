5d3bb5f92c9f565c9a696d57d934daed295e73f4
Thomas Perkins <thomasperkins1123@gmail.com>
2020-04-30 19:57:07 +0100

    Make dogfooding make pr instead

--- .codebuild/dogfooding-demo.sh
+++ .codebuild/dogfooding-demo.sh
@@ -4,17 +4,20 @@ set -euvo pipefail
 
 BRANCH="$1"
 SHA="$2"
-mvn clean compile -B 
-timeout 360 ./dcover create -d src/dcover/java --skip-test-verification --batch
-python3 .codebuild/removeTrivialTests.py
 
 if [ "$BRANCH" != "dcover-dogfooding-noTrivial" ]
 then
-	git add src/dcover
-	git commit -m "Add diffblue tests" || true
-	git push origin "$BRANCH"
-	NEW_SHA=$(git rev-parse HEAD)
-	echo "$SHA"
-	echo "$NEW_SHA"
-	.codebuild/github-post-pr.sh "$SHA" "Diffblue has found some code changes please check these are intended $NEW_SHA"
+	if [[ $BRANCH != *"-Diffblue-tests"* ]]; then
+		git checkout -b "$BRANCH""-Diffblue-tests"
+		mvn clean compile -B 
+		timeout 360 ./dcover create -d src/dcover/java --skip-test-verification --batch
+		python3 .codebuild/removeTrivialTests.py
+		git add src/dcover
+		git commit -m "Add diffblue tests" || true
+		git push origin "$BRANCH""-Diffblue-tests"
+		NEW_SHA=$(git rev-parse HEAD)
+		echo "$SHA"
+		echo "$NEW_SHA"
+		.codebuild/github-post-pr.sh "$SHA" "Diffblue has found some code changes please check these are intended https://github.com/diffblue/CoreBanking/compare/$BRANCH...$BRANCH-Diffblue-tests?expand=1"
+        fi
 fi
