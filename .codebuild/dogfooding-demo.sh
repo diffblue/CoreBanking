05599d044d036d4857eef5c77e758205f3005ee4
Thomas Perkins <thomasperkins1123@gmail.com>
2020-05-01 13:15:28 +0100

    Make ci run different again

--- .codebuild/dogfooding-demo.sh
+++ .codebuild/dogfooding-demo.sh
@@ -8,16 +8,22 @@ SHA="$2"
 if [ "$BRANCH" != "dcover-dogfooding-noTrivial" ]
 then
 	if [[ $BRANCH != *"-Diffblue-tests"* ]]; then
-		git checkout -b "$BRANCH""-Diffblue-tests"
-		mvn clean compile -B 
-		timeout 360 ./dcover create -d src/dcover/java --skip-test-verification --batch
-		python3 .codebuild/removeTrivialTests.py
-		git add src/dcover
-		git commit -m "Add diffblue tests" || true
-		git push origin "$BRANCH""-Diffblue-tests"
-		NEW_SHA=$(git rev-parse HEAD)
-		echo "$SHA"
-		echo "$NEW_SHA"
-		.codebuild/github-post-pr.sh "$SHA" "Diffblue has found some code changes please check these are intended https://github.com/diffblue/CoreBanking/compare/$BRANCH...$BRANCH-Diffblue-tests?expand=1"
+		lastUser=$(git log -1 --pretty=format:'%an')
+		echo "$lastUser"
+		if [ "$lastUser" == Diffblue Cover Bot ]; then
+			mvn clean test
+		else
+			git checkout -b "$BRANCH""-Diffblue-tests"
+			mvn clean compile -B 
+			timeout 360 ./dcover create -d src/dcover/java --skip-test-verification --batch
+			python3 .codebuild/removeTrivialTests.py
+			git add src/dcover
+			git commit -m "Add diffblue tests" || true
+			git push origin "$BRANCH""-Diffblue-tests"
+			NEW_SHA=$(git rev-parse HEAD)
+			echo "$SHA"
+			echo "$NEW_SHA"
+			.codebuild/github-post-pr.sh "$SHA" "Diffblue has found some code changes please check these are intended https://github.com/diffblue/CoreBanking/compare/$BRANCH...$BRANCH-Diffblue-tests?expand=1"
+		fi
         fi
 fi
