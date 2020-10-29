5d6fd8d3c73cf21bafa3ea6eb8e4865b7840ee7e
thomasperkins1123 <thomasperkins1123@gmail.com>
2020-10-29 10:54:16 +0000

    Fix create account

--- src/main/java/io/diffblue/corebanking/ui/menu/AccountsMenu.java
+++ src/main/java/io/diffblue/corebanking/ui/menu/AccountsMenu.java
@@ -2,6 +2,11 @@ package io.diffblue.corebanking.ui.menu;
 
 import io.diffblue.corebanking.account.Account;
 import io.diffblue.corebanking.CoreBanking;
+import io.diffblue.corebanking.client.Client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class AccountsMenu extends Menu {
   private static final String MENU_TITLE = "Accounts Menu";
@@ -52,5 +57,28 @@ public class AccountsMenu extends Menu {
    * Takes the input required to create a new account, and creates it, adding it to the CoreBanking
    * instance.
    */
-  private void createNewAccount() {}
+  private void createNewAccount() {
+    System.out.println("Enter existing client name: ");
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    try {
+      String clientName = reader.readLine();
+      coreBanking.openNewAccount(getClientFromName(clientName), 0);
+    } catch (IOException e) {
+      System.out.println("Client name invalid.");
+    } catch (NullPointerException e) {
+      System.out.println("Client name does not exist");
+    }
+  }
+
+  /**
+   * Take client name as a string and returns the client object for that client
+   */
+  private Client getClientFromName(String clientName){
+    for (Client client: coreBanking.getClients()) {
+      if (client.getClientName().equals(clientName)) {
+        return client;
+      }
+    }
+    return null;
+  }
 }
