afdd70d9013b33eb16f010b1b24f94d9c701555c
Peter Schrammel <peter.schrammel@diffblue.com>
2021-02-21 16:33:45 +0000

    Clean up run-router.sh

--- run-router.sh
+++ run-router.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-COVER_CLI_ROOT="path/to/uncompressed/cover-cli/zip"
+AGENT_JAR=diffblue-cover/cover-replay-agent.jar
 
-AGENT="$COVER_CLI_ROOT/cover-replay-agent.jar"
-
-export COVER_TARGET=io.diffblue.corebanking.
-
-set -x
-exec java -cp 'target/classes:target/dependency/*' -javaagent:$AGENT io.diffblue.corebanking.ui.service.MessageRouterService
+# run the service router
+java -ea -cp 'target/classes:target/dependency/*' -javaagent:$AGENT_JAR io.diffblue.corebanking.ui.service.MessageRouterService
