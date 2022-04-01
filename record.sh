c8c30b0aa341c11ba38b868cd2585be5bc9f9eb1
Joel Allred <joel.allred@diffblue.com>
2022-04-01 13:53:25 +0100

    Add more info to record.sh
    So that we can simplify the website docs.

--- record.sh
+++ record.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+# Agent that will record the execution
 AGENT=$HOME/cover/cover-replay-agent.jar # MODIFY HERE IF NECESSARY!
 
-# proactively check for common mistakes when running the demo
+# Proactively check for common mistakes when running the demo
 if [ ! -f "$AGENT" ]; then
-  echo "File '$AGENT' not found: please edit line 3 of the script '$0'"
+  echo "File '$AGENT' not found: please edit line 4 of the script '$0'"
   exit 1
 fi
 if [ ! -d target/classes ]; then
@@ -16,10 +17,17 @@ if [ ! -d target/dependency ]; then
   exit 1
 fi
 
-# figure out the classpath separator, ":" in unix and ";" in windows
+# Figure out the classpath separator, ":" in unix and ";" in windows
 SEP=$(java -XshowSettings 2>&1 | grep path.separator | sed 's/.*path.separator = //')
 
-# record the application under test
 set -x
+
+# The $COVER_TARGET environment variable instructs the agent to record just enough to extract tests for classes that live in the package `io.diffblue`.
 export COVER_TARGET=io.diffblue.
+
+# Record the application under test
+
+# The -javagent argument must be put before the class, otherwise it would be
+# passed to the application instead of the JVM and the application would not
+# be recorded.
 java -javaagent:$AGENT -cp "target/classes${SEP}target/dependency/*" io.diffblue.corebanking.ui.batch.BatchProcessor src/test/resources/batch/*.txt
