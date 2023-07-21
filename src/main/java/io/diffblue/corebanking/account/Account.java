c5db184244527cb6f9d0a9a224828166ae47d2e6
janewalls <walls.jane@outlook.com>
2023-07-21 17:13:40 +0100

    useless comment

--- src/main/java/io/diffblue/corebanking/account/Account.java
+++ src/main/java/io/diffblue/corebanking/account/Account.java
@@ -6,6 +6,7 @@ import io.diffblue.corebanking.transaction.Transaction;
 import java.util.ArrayList;
 import java.util.List;
 
+// Useless comment
 /** Account class. */
 public class Account {
   public static final int ACCOUNT_NUMBER_LENGTH = 4;
